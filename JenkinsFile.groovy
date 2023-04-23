21d5576136297f8032f6e4e21e4723b8ead4f373
R.Dasarathi <dasarathi.rout@live.com>
2023-04-23 20:45:50 +0530

    SharedLib Custom Groovy Call M-005

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,40 +1,6 @@
 @Library('CICD-Shared-Libraries@main') _
-
-pipeline {
-
-    agent any
-
-    environment {
-        USER_ENV_VAR = "TEST VALUE USER ENV VALUE"
-    }//environment
-
-    stages {
-
-        stage("CLONE") {
-            steps {
-                println("Running Git Clone")
-                BuildPipeline {
-                    projectName = 'TEST-PROJECT'
-                }
-            }
-        }
-
-        stage("BUILD") {
-            steps {
-                println("Running Gradle Build")
-            }
-        }
-
-        stage("PUBLISH") {
-            steps {
-                println("Running Gradle BootJar & Docker Image Publish")
-            }
-        }
-
-        stage("DEPLOY") {
-            steps {
-                println("Running Docker Image K8S Deploy")
-            }
-        }
-    }// stages
-}//pipeline
+node {
+    BuildPipeline {
+        projectName = 'TEST-PROJECT'
+    }// BuildPipeline
+}//node
