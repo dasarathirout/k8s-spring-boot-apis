342f5f7ab4443a408b290b3850aa8b0840a45a4d
R.Dasarathi <dasarathi.rout@live.com>
2023-04-23 18:38:17 +0530

    SharedLib Groovy Call M-004

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,35 +1,46 @@
+@Library('CICD-Shared-Libraries@main') _
 
 pipeline {
-    
+
     agent any
-    
+
     environment {
         USER_ENV_VAR = "TEST VALUE USER ENV VALUE"
     }//environment
 
-    stages {
-        stage("CLONE") {
-            steps {
-                println("Running Git Clone")
-            }
-        }
+    ansiColor('xterm') {
+        timeout(60) {
+            timestamps {
+                stages {
+                    stage('CLEAN') {
+                        step([$class: 'WsCleanup'])
+                    }
+                    stage("CLONE") {
+                        steps {
+                            println("Running Git Clone")
+                            BuildPipeline.startJobBuild('TEST-PROJECT')
+                        }
+                    }
 
-        stage("BUILD") {
-            steps {
-                println("Running Gradle Build")
-            }
-        }
+                    stage("BUILD") {
+                        steps {
+                            println("Running Gradle Build")
+                        }
+                    }
 
-        stage("PUBLISH") {
-            steps {
-                println("Running Gradle BootJar & Docker Image Publish")
-            }
-        }
+                    stage("PUBLISH") {
+                        steps {
+                            println("Running Gradle BootJar & Docker Image Publish")
+                        }
+                    }
 
-        stage("DEPLOY") {
-            steps {
-                println("Running Docker Image K8S Deploy")
-            }
-        }
-    }//stages
+                    stage("DEPLOY") {
+                        steps {
+                            println("Running Docker Image K8S Deploy")
+                        }
+                    }
+                }//stages
+            }// timestamps
+        }// timeout
+    }// ansiColor
 }//pipeline
